3d9ff7dbcaf2b79898d4ee372d52b9a11c6589b0
rgparker1689 <81049293+rgparker1689@users.noreply.github.com>
2021-06-15 00:56:38 -0400

    commenting update

--- groove-ml.py
+++ groove-ml.py
@@ -31,23 +31,23 @@ for i in labels:
     audiowavs[i] = s
 
 # outputs waveforms to comet
-# wvfrms = plt.figure(figsize=(15, 15))
-# experiment.log_image('waveforms.png')
-# wvfrms.subplots_adjust(hspace=0.4, wspace=0.4)
-# index_counter = 0  # prevents overlap on png
-# for i, label in enumerate(labels):
-#     for j in range(audiowavs[label].size):
-#         func = audiowavs[label].loc[j]
-#         wvfrms.add_subplot(10, 2, index_counter + 1)
-#         plt.title(label + ": " + func[50:])
-#         y, sample_rate = librosa.load(func)
-#         librosa.display.waveplot(y, sr=sample_rate)
-#         index_counter += 1
-#         experiment.log_audio(func, metadata={'name': label})  # logs wav
-# plt.savefig('waveforms.png')
-#experiment.log_image('waveforms.png')
+wvfrms = plt.figure(figsize=(15, 15))
+experiment.log_image('waveforms.png')
+wvfrms.subplots_adjust(hspace=0.4, wspace=0.4)
+index_counter = 0  # prevents overlap on png
+for i, label in enumerate(labels):
+    for j in range(audiowavs[label].size):
+        func = audiowavs[label].loc[j]
+        wvfrms.add_subplot(10, 2, index_counter + 1)
+        plt.title(label + ": " + func[50:])
+        y, sample_rate = librosa.load(func)
+        librosa.display.waveplot(y, sr=sample_rate)
+        index_counter += 1
+        experiment.log_audio(func, metadata={'name': label})  # logs wav
+plt.savefig('waveforms.png')
+experiment.log_image('waveforms.png')
 
-# outputs tempograms/estimates to comet
+# outputs tempogram/bpm estimate graphs to comet
 prior = scipy.stats.uniform(30, 300) # ASSUMES BPM IN 30,300
 tmpgrms = plt.figure(figsize=(15, 15))
 experiment.log_image('tempograms.png')
@@ -68,7 +68,7 @@ for i, label in enumerate(labels):
 plt.savefig('tempograms.png')
 experiment.log_image('tempograms.png')
 
-# outputs autocorrelations to comet
+# outputs autocorrelation graphs to comet
 acrlts = plt.figure(figsize=(15, 15))
 experiment.log_image('autocorrelations.png')
 acrlts.subplots_adjust(hspace=1.4, wspace=0.4)
@@ -94,7 +94,7 @@ def gather_autocor(path, hop):
     y, sr = librosa.load(path=path, duration=3)
     oenv = librosa.onset.onset_strength(y=y, sr=sr, hop_length=hop)
     tempogram = librosa.feature.tempogram(onset_envelope=oenv, sr=sr, hop_length=hop) # local autocorrelation
-    ac_global = librosa.autocorrelate(oenv, max_size=tempogram.shape[0])
+    ac_global = librosa.autocorrelate(oenv, max_size=tempogram.shape[0]) # global autocorrelation
     return tempogram, ac_global
 
 
@@ -109,13 +109,11 @@ for label in labels:
         features.append([local_ac, global_ac, label])
 featuresdf = pd.DataFrame(features, columns=['local', 'global', 'label'])
 
-x1 = np.mean(featuresdf['local'].tolist(), axis=1)
-x2 = np.array(featuresdf['global'].tolist())
+x1 = np.mean(featuresdf['local'].tolist(), axis=1) # averaging local autocorrelations for each audio file to match shape of global
+x2 = np.array(featuresdf['global'].tolist()) # reformatting global array into manageable shape ^
 y = featuresdf['label'].tolist()
 le = LabelEncoder()
 yy = tf.utils.to_categorical(le.fit_transform(y))
-print(x1.shape)
-print(x2.shape)
 np.savetxt("x1.csv", x1, delimiter=",")
 np.savetxt("x2.csv", x2, delimiter=",", fmt='%s')
 
