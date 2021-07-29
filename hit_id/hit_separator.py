890469075ed92c5f9216d99ebde850f5e7348f9b
Riley Parker <rgparker1689@gmail.com>
2021-07-29 11:59:45 -0400

    Update hit_separator.py

--- hit_id/hit_separator.py
+++ hit_id/hit_separator.py
@@ -12,7 +12,7 @@ def separate(y, sr):
     hoplength = int(sr/1000)
     margin_adj = hoplength*3
     onsets = librosa.onset.onset_strength(y=y, sr=sr, hop_length=hoplength)
-    onsets -= 1.5*np.average(onsets)
+    onsets -= 2.7*np.average(onsets)
     onsets[onsets < 0] = 0
     onsets /= np.max(onsets)
     hpl2 = hoplength
@@ -65,18 +65,19 @@ def preprocess(y, sr, name='default'):
                 nonzero = True
 
     # either crop or elongate to normalize
-    if y.shape[0] >= 3000:
+    if y.shape[0] >= 3000:  # no need to append zeros -- logic just centers bounds of hit when possible
         if max_index >= 1499:
             if y.shape[0] - max_index <= 1500:
                 end_index = y.shape[0] - 1
                 start_index = max_index - (1500 + (1501 - (y.shape[0] - max_index)))
             else:
                 start_index = max_index - 1500
+                print('ideal found')
                 end_index = max_index + 1500
         else:
             start_index = 0
             end_index = max_index + (3000 - max_index)
-    else:
+    else:  # decides where to append zeros when series too short -- at beginning and at end, respectively
         if max_index <= 1500:
             start_index = 0
             y = np.append(np.zeros(1500-max_index), y)
@@ -85,15 +86,16 @@ def preprocess(y, sr, name='default'):
             start_index = max_index - 1500
         if y.shape[0] - max_index <= 1500:
             y = np.append(y, np.zeros(1500 - (y.shape[0] - max_index)))
+            onsets = np.append(onsets, np.zeros(int(1500/16) - (onsets.shape[0] - int(max_index/16)) + 2))
             end_index = y.shape[0]
         else:
             end_index = max_index + 1500
-        print("start and end indices: ")
-        print(start_index, end_index)
+        print("zeros successfully appended!")
     bounds = np.array([start_index, end_index])
+    y = y[start_index:end_index]
 
     # plotting
-    times = librosa.times_like(onsets, sr=sr, hop_length=10)
+    times = librosa.times_like(onsets, sr=sr, hop_length=16)
     fig1 = plt.figure()
     ax1 = fig1.add_subplot()
     plt.sca(ax1)
@@ -102,21 +104,37 @@ def preprocess(y, sr, name='default'):
     onset_bounds = (bounds/16).astype(int)
     ax1.vlines(times[onset_max], 0, onsets.max(), color='r', alpha=0.7)
     ax1.vlines(times[onset_bounds], 0, onsets.max(), color='green', alpha=0.7)
+
+    # if multiple peaks detected, log an image for debug
     if mult_peaks:
         plt.savefig(name + '.png')
-        plt.close(fig1)
-        return True, y
     plt.close(fig1)
-    y = y[start_index:end_index]
-    print(y.shape[0])
-    return False, y
+    if y.shape[0] != 3000:
+        print(y.shape[0])
+    return mult_peaks, y
 
 
-y, sr = librosa.load("audios/1_4a120.wav")
-temp_hits = separate(y, sr)
-total_wrong = 0
-for i in temp_hits:
-    mult_peaks, y = preprocess(i, sr, str(total_wrong))
+# function performs both of above and returns a numpy array composed of determined hits
+def get_hits(path):
+    y, sr = librosa.load(path)
+    temp_hits = separate(y, sr)
+    hits = []
+    mult_instances = 0
+    mult_peaks = False
+
+    # creating list of separated hit time series
+    for i in temp_hits:
+        mult_det, hit = preprocess(i, sr, str(mult_instances))
+        if mult_det:
+            mult_peaks = True
+            mult_instances += 1
+        hits.append(hit)
+
     if mult_peaks:
-        total_wrong += 1
-print(str(total_wrong) + '/' + str(len(temp_hits)))
+        print("Warning: multiple peaks were detected in {} segments.".format(mult_instances))
+        print("This may result in {}+ undetected cymbals or drums or improper classification.".format(mult_instances))
+    return np.asarray(hits)
+
+
+the_array = get_hits("audios/1_4rc100.wav")
+print(the_array.shape)
