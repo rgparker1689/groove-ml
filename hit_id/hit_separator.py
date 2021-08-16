5c3eced280e516aabbde1b6c71cdee8b8dfae22f
Riley Parker <rgparker1689@gmail.com>
2021-08-15 23:27:19 -0500

    Update hit_separator.py
    working on more accurate separation/testing output

--- hit_id/hit_separator.py
+++ hit_id/hit_separator.py
@@ -1,9 +1,8 @@
-import pandas as pd
 import numpy as np
 import librosa
 import librosa.display
+import soundfile
 import matplotlib.pyplot as plt
-from pathlib import Path
 
 
 # separates time series into list of segments representing hits
@@ -12,20 +11,21 @@ def separate(y, sr):
     hoplength = int(sr/1000)
     margin_adj = hoplength*3
     onsets = librosa.onset.onset_strength(y=y, sr=sr, hop_length=hoplength)
-    onsets -= 2.7*np.average(onsets)
+    onsets -= np.average(onsets)
     onsets[onsets < 0] = 0
     onsets /= np.max(onsets)
     hpl2 = hoplength
     peaks = librosa.util.peak_pick(onsets, hpl2, hpl2, hpl2, hpl2, .01, hpl2)
     times = librosa.times_like(onsets, sr=sr, hop_length=hoplength)
+    # beats = (gg.beat_placement(y=y, sr=sr, bpm=100)/hoplength).astype(int)
 
     # plotting work
     fig1 = plt.figure()
     ax1 = fig1.add_subplot()
     plt.sca(ax1)
-    limits = ax1.get_ylim()
     ax1.plot(times, onsets, label="Onset Strength", alpha=0.6)
     ax1.vlines(times[peaks], 0, onsets.max(), color='r', alpha=0.4, label="Peaks")
+    # ax1.vlines(times[beats], 0, onsets.max(), color='g', alpha=0.6, label="Beat Estimate")
     ax1.legend(frameon=True, framealpha=0.8)
     plt.savefig("temp.png")
     plt.close(fig1)
@@ -38,12 +38,13 @@ def separate(y, sr):
         finish = (peaks[i+1] + margin_adj) * hoplength
         hit = y[start:finish]
         hits.append(hit)
-    return hits
+    return hits, hoplength*peaks[1:]
 
 
 # processes a time series containing a single drum or cymbal hit into a normalized format for interpretation
-def preprocess(y, sr, name='default'):
+def preprocess(y, sr, name='default', width=3000, dump=False):
     # begin with search for multiple peaks (invalid input)
+    half = width//2
     onsets = librosa.onset.onset_strength(y=y, sr=sr, hop_length=16)
     onsets -= 4*np.average(onsets)
     onsets[onsets < 0] = 0
@@ -65,31 +66,31 @@ def preprocess(y, sr, name='default'):
                 nonzero = True
 
     # either crop or elongate to normalize
-    if y.shape[0] >= 3000:  # no need to append zeros -- logic just centers bounds of hit when possible
-        if max_index >= 1499:
-            if y.shape[0] - max_index <= 1500:
+    if y.shape[0] >= width:  # no need to append zeros -- logic just centers bounds of hit when possible
+        if max_index >= half - 1:
+            if y.shape[0] - max_index <= half:
                 end_index = y.shape[0] - 1
-                start_index = max_index - (1500 + (1501 - (y.shape[0] - max_index)))
+                start_index = max_index - (half + (half + 1 - (y.shape[0] - max_index)))
             else:
-                start_index = max_index - 1500
+                start_index = max_index - half
                 print('ideal found')
-                end_index = max_index + 1500
+                end_index = max_index + half
         else:
             start_index = 0
-            end_index = max_index + (3000 - max_index)
+            end_index = max_index + (half - max_index)
     else:  # decides where to append zeros when series too short -- at beginning and at end, respectively
-        if max_index <= 1500:
+        if max_index <= width//2:
             start_index = 0
-            y = np.append(np.zeros(1500-max_index), y)
-            max_index += (1500 - max_index)
+            y = np.append(np.zeros(half - max_index), y)
+            max_index += (half - max_index)
         else:
-            start_index = max_index - 1500
-        if y.shape[0] - max_index <= 1500:
-            y = np.append(y, np.zeros(1500 - (y.shape[0] - max_index)))
-            onsets = np.append(onsets, np.zeros(int(1500/16) - (onsets.shape[0] - int(max_index/16)) + 2))
+            start_index = max_index - half
+        if y.shape[0] - max_index <= half:
+            y = np.append(y, np.zeros(half - (y.shape[0] - max_index)))
+            onsets = np.append(onsets, np.zeros(int(half/16) - (onsets.shape[0] - int(max_index/16)) + 2))
             end_index = y.shape[0]
         else:
-            end_index = max_index + 1500
+            end_index = max_index + half
         print("zeros successfully appended!")
     bounds = np.array([start_index, end_index])
     y = y[start_index:end_index]
@@ -106,25 +107,26 @@ def preprocess(y, sr, name='default'):
     ax1.vlines(times[onset_bounds], 0, onsets.max(), color='green', alpha=0.7)
 
     # if multiple peaks detected, log an image for debug
-    if mult_peaks:
-        plt.savefig(name + '.png')
+    if mult_peaks or dump:
+        plt.savefig('imgs/' + name + '.png')
+        soundfile.write('outputs/' + name + '.wav', y, sr)
     plt.close(fig1)
-    if y.shape[0] != 3000:
+    if y.shape[0] != width:
         print(y.shape[0])
-    return mult_peaks, y
+    return mult_peaks or dump, y
 
 
 # function performs both of above and returns a numpy array composed of determined hits
 def get_hits(path):
     y, sr = librosa.load(path)
-    temp_hits = separate(y, sr)
+    temp_hits, indices = separate(y, sr)
     hits = []
     mult_instances = 0
     mult_peaks = False
 
     # creating list of separated hit time series
     for i in temp_hits:
-        mult_det, hit = preprocess(i, sr, str(mult_instances))
+        mult_det, hit = preprocess(i, sr, str(mult_instances), width=3000, dump=True)
         if mult_det:
             mult_peaks = True
             mult_instances += 1
@@ -133,8 +135,11 @@ def get_hits(path):
     if mult_peaks:
         print("Warning: multiple peaks were detected in {} segments.".format(mult_instances))
         print("This may result in {}+ undetected cymbals or drums or improper classification.".format(mult_instances))
-    return np.asarray(hits)
+        print("Note: ignore if dump parameter is set to True")
+    else:
+        print("All detected peaks successfully isolated!")
+    return np.asarray(hits), indices
 
-
-the_array = get_hits("audios/1_4rc100.wav")
-print(the_array.shape)
+hits2, idx = get_hits('audios/1_2rc110.wav')
+print(len(hits2))
+print(hits2[0].shape)
